4ad1834dc827d9fbc62bb0f1905c25de6461bfa4
Julio Morales <julioamb11@gmail.com>
2023-12-29 10:43:41 -0500

    PSFull create profile functionality for Trainee and Trainer

---
+++ src/main/java/com/epam/gymcrm/GymcrmApplication.java
@@ -0,0 +1,28 @@
+package com.epam.gymcrm;
+
+
+import org.modelmapper.ModelMapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class GymcrmApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymcrmApplication.class, args);
+
+
+	}
+
+
+
+}
+
---
+++ src/main/java/com/epam/gymcrm/domain/model/Trainer.java
@@ -0,0 +1,53 @@
+package com.epam.gymcrm.domain.model;
+
+import lombok.Builder;
+
+import java.util.ArrayList;
+
+@Builder
+public class Trainer {
+
+    private Integer trainerId;
+
+    private String specialization;
+
+    private Integer userId;
+
+    private ArrayList<Trainee> trainees;
+
+    public Trainer(Integer trainerId, String specialization, Integer userId, ArrayList<Trainee> trainees) {
+        this.trainerId = trainerId;
+        this.specialization = specialization;
+        this.userId = userId;
+        this.trainees = new ArrayList<>();
+    }
+
+    public Trainer(){
+
+    }
+
+
+    public Integer getTrainerId() {
+        return trainerId;
+    }
+
+    public void setTrainerId(Integer trainerId) {
+        this.trainerId = trainerId;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+}
